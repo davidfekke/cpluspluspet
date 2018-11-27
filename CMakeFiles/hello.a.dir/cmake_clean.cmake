860b1862755408a91023ebf521c2c5b63da7af22
davidfekke <david@fekke.com>
2018-11-26 21:03:10 -0500

    Adding CMakeLists.txt file

---
+++ CMakeFiles/hello.a.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/hello.a.dir/helloworld.cpp.o"
+  "CMakeFiles/hello.a.dir/Pet.cpp.o"
+  "hello.a.pdb"
+  "hello.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/hello.a.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ deletelinkfiles.sh
@@ -0,0 +1,3 @@
+[ -f helloworld.o ] && rm helloworld.o
+[ -f Pet.o ] && rm Pet.o 
+[ -f hello ] && rm hello
